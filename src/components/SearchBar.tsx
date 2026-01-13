2b6f6ba72c760c9351eee8222e97cf5786e5f8e8
Katie <kymlu.4@gmail.com>
2026-01-13 20:32:37 +0900

    add search bar icon

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Icon from "./Icon";
+import { ICON } from "../data/icons";
 
 export default function SearchBar(props: {
   onContentChange: (value: string) => void
@@ -10,12 +12,17 @@ export default function SearchBar(props: {
     props.onContentChange(newValue);
   }
 
-  return <input
-    type="search"
-    name={"検索"}
-    placeholder={"探す"}
-    value={value}
-    onInput={(event) => handleChange(event.currentTarget.value)}
-    className="w-full col-start-1 row-start-1 px-4 py-2 mb-2 text-gray-700 border-2 border-gray-700 rounded-md focus-within:border-primary focus:outline-none"
-  />
+  return <div className="flex">
+    <input
+      type="search"
+      name={"検索"}
+      placeholder={"探す"}
+      value={value}
+      onInput={(event) => handleChange(event.currentTarget.value)}
+      className="w-full col-start-1 row-start-1 py-2 pl-12 pr-4 mb-2 text-gray-700 border-2 border-gray-700 rounded-md focus-within:border-primary focus:outline-none"
+    />
+    <div className="absolute p-2">
+      <Icon src={ICON.search} alt="検索バー"/>
+    </div>
+  </div>
 }
--- src/data/icons.ts
+++ src/data/icons.ts
@@ -4,4 +4,5 @@ export const ICON = {
   favourite_filled: "icons/favourite_filled.svg",
   favourite_outline: "icons/favourite_outline.svg",
   heart_minus: "icons/heart_minus.svg",
+  search: "icons/search.svg",
 }
